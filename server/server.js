3d2a1dd4b70548cd706175873f8dc606760dfa36
Deepansu <itzdeepansu@gmail.com>
2024-06-28 15:18:20 +0530

    cors-1

--- server/server.js
+++ server/server.js
@@ -1,51 +1,47 @@
 require("dotenv").config();
 const axios = require("axios");
-try {
-  const io = require("socket.io")(3000, {
-    cors: {
-      origin: process.env.FRONTEND_URL,
-      methods: ["GET", "POST"],
-      credentials: true,
-    },
+const io = require("socket.io")(3000, {
+  cors: {
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+let sidMap = new Map();
+io.on("connection", (socket) => {
+  console.log("User Connected", socket.id);
+  socket.on("sendSocketID", (data) => {
+    // try {
+    //   axios.post(`${process.env.API_URL}user/setsocketid`, {
+    //     phoneNumber: data.phoneNumber,
+    //     socketID: data.socketID,
+    //   });
+    // } catch (err) {
+    //   console.log("error at setting socket id");
+    // }
+    sidMap.set(socket.id, data.phoneNumber);
   });
-  let sidMap = new Map();
-  io.on("connection", (socket) => {
-    console.log("User Connected", socket.id);
-    socket.on("sendSocketID", (data) => {
-      // try {
-      //   axios.post(`${process.env.API_URL}user/setsocketid`, {
-      //     phoneNumber: data.phoneNumber,
-      //     socketID: data.socketID,
-      //   });
-      // } catch (err) {
-      //   console.log("error at setting socket id");
-      // }
-      sidMap.set(socket.id, data.phoneNumber);
-    });
 
-    socket.on("sendMessage", (data) => {
-      console.log(data.sender, "---- message ----", data.receiver);
-      socket.to(data.receiver).emit("recieveMessage", data);
-      console.log(data, "recieved");
-    });
-
-    socket.on("deleteMessage", (data) => {
-      socket.to(data.receiver).emit("deleteReceiveMessage", data);
-    });
-    socket.on("disconnect", async () => {
-      const phoneNumber = sidMap.get(socket.id);
-
-      try {
-        await axios.post(`${process.env.API_URL}user/setoffline`, {
-          phoneNumber: phoneNumber,
-        });
-        console.log("User Disconnected", socket.id);
-      } catch (error) {
-        console.error("Error setting user offline:");
-      }
-      sidMap.delete(socket.id);
-    });
+  socket.on("sendMessage", (data) => {
+    console.log(data.sender, "---- message ----", data.receiver);
+    socket.to(data.receiver).emit("recieveMessage", data);
+    console.log(data, "recieved");
   });
-} catch (err) {
-  console.log("Error setting connection", err);
-}
+
+  socket.on("deleteMessage", (data) => {
+    socket.to(data.receiver).emit("deleteReceiveMessage", data);
+  });
+  socket.on("disconnect", async () => {
+    const phoneNumber = sidMap.get(socket.id);
+
+    try {
+      await axios.post(`${process.env.API_URL}user/setoffline`, {
+        phoneNumber: phoneNumber,
+      });
+      console.log("User Disconnected", socket.id);
+    } catch (error) {
+      console.error("Error setting user offline:");
+    }
+    sidMap.delete(socket.id);
+  });
+});
