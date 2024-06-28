7711aa4d01daad9ed767d54dfbb03a1e15f32ca2
Deepansu <itzdeepansu@gmail.com>
2024-06-28 13:25:57 +0530

    findmany -4

--- client/app/api/user/getusers/route.jsx
+++ client/app/api/user/getusers/route.jsx
@@ -1,7 +1,11 @@
 import { NextResponse } from "next/server";
 import prisma from "@/app/libs/prismadb";
 
-export async function GET(){
-    const users = await prisma.user.findMany();
-    return NextResponse.json({users});
+export async function GET() {
+  const users = await prisma.user.findMany({
+    where: {
+      onlineStatus: true,
+    },
+  });
+  return NextResponse.json({ users });
 }
