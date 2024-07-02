de76a8cde7af607dcbb2dce96bbb955ca218dc22
Deepansu <itzdeepansu@gmail.com>
2024-07-02 12:10:29 +0530

    express-2

--- server/server.js
+++ server/server.js
@@ -1,56 +1,60 @@
-require('dotenv').config();
-const express = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
-const cors = require('cors');
-const axios = require('axios');
+import express from "express";
+import { Server } from "socket.io";
+import { createServer } from "http";
+import cors from "cors";
+
+const port = 3000;
 
 const app = express();
-const server = http.createServer(app);
+const server = createServer(app);
+
 const io = new Server(server, {
   cors: {
-    origin: '*',
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type', 'X-Auth-Token', 'Origin', 'Authorization'],
+    origin: "*",
+    methods: ["GET", "POST"],
     credentials: true,
   },
 });
 
 app.use(cors());
 
-let sidMap = new Map();
-io.on('connection', (socket) => {
-  console.log('User Connected', socket.id);
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
 
-  socket.on('sendSocketID', (data) => {
+let sidMap = new Map();
+io.on("connection", (socket) => {
+  console.log("User Connected", socket.id);
+
+  socket.on("sendSocketID", (data) => {
     sidMap.set(socket.id, data.phoneNumber);
   });
 
-  socket.on('sendMessage', (data) => {
-    console.log(data.sender, '---- message ----', data.receiver);
-    socket.to(data.receiver).emit('recieveMessage', data);
-    console.log(data, 'recieved');
+  socket.on("sendMessage", (data) => {
+    console.log(data.sender, "---- message ----", data.receiver);
+    socket.to(data.receiver).emit("recieveMessage", data);
+    console.log(data, "recieved");
   });
 
-  socket.on('deleteMessage', (data) => {
-    socket.to(data.receiver).emit('deleteReceiveMessage', data);
+  socket.on("deleteMessage", (data) => {
+    socket.to(data.receiver).emit("deleteReceiveMessage", data);
   });
 
-  socket.on('disconnect', async () => {
+  socket.on("disconnect", async () => {
     const phoneNumber = sidMap.get(socket.id);
 
     try {
       await axios.post(`${process.env.API_URL}user/setoffline`, {
         phoneNumber: phoneNumber,
       });
-      console.log('User Disconnected', socket.id);
+      console.log("User Disconnected", socket.id);
     } catch (error) {
-      console.error('Error setting user offline:');
+      console.error("Error setting user offline:");
     }
     sidMap.delete(socket.id);
   });
 });
 
-server.listen(process.env.PORT || 3000, () => {
-  console.log(`Server running on port ${process.env.PORT || 3000}`);
+server.listen(port, () => {
+  console.log(`Server running on ${port}`);
 });
