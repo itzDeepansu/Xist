a1658c34c15e07591e46a91e95f02e5f0142bead
Deepansu <itzdeepansu@gmail.com>
2024-07-02 11:03:34 +0530

    please

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -23,7 +23,7 @@ import { useSession, signOut } from "next-auth/react";
 
 export default function Home() {
   const router = useRouter();
-  const socket = useMemo(() => io(`${process.env.BACKE}`), []);
+  const socket = useMemo(() => io(`${process.env.BACKEND_URL}`), []);
 
   const [profile, setProfile] = useState(null);
 
